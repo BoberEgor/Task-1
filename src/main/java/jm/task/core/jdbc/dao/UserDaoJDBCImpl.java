515ba1500aa94eff25c17d3c79f88bf0f13f5dab
BoberEgor <145572715+BoberEgor@users.noreply.github.com>
2025-02-01 22:49:24 +0600

    jdbc

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,102 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import java.sql.*;
+import java.util.*;
 
-import java.util.List;
+import static jm.task.core.jdbc.util.Util.getJdbcConnection;
 
 public class UserDaoJDBCImpl implements UserDao {
+
     public UserDaoJDBCImpl() {
-
     }
 
+    @Override
     public void createUsersTable() {
-
+        String sql = Util.getProperty("jdbcCreateUsersTable");
+        try (Connection connection = getJdbcConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+            System.out.println("Table 'users' created");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+    @Override
     public void dropUsersTable() {
-
+        String sql = Util.getProperty("jdbcDropUsersTable");
+        try (Connection connection = getJdbcConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+            System.out.println("Table 'users' dropped");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        String sql = Util.getProperty("jdbcInsertUser");
+        try (Connection connection = getJdbcConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+            System.out.println("User " + name + " " + lastName + " added to the database.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+    @Override
     public void removeUserById(long id) {
-
+        String sql = Util.getProperty("jdbcRemoveUserById");
+        try (Connection connection = getJdbcConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+            System.out.println("User with ID " + id + " removed.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+    @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        String sql = Util.getProperty("jdbcGetAllUsers");
+        try (Connection connection = getJdbcConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+
+            while (resultSet.next()) {
+                Long id = resultSet.getLong("id");
+                String name = resultSet.getString("name");
+                String lastName = resultSet.getString("last_name");
+                Byte age = resultSet.getByte("age");
+
+                User user = new User(name, lastName, age);
+                user.setId(id);
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
     }
 
+    @Override
     public void cleanUsersTable() {
-
+        String sql = Util.getProperty("jdbcCleanUsersTable");
+        try (Connection connection = getJdbcConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+            System.out.println("Users table cleaned");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
